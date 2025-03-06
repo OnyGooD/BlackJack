4005fa49ca51c455b65de6c8d08f75bd43468e50
OnyGooD <GooDy>
2025-03-06 12:45:48 +0100

    Add functionality to display picked cards and enable button interactions

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
 const BASE_URL = "https://deckofcardsapi.com/api/deck"
-const newDeckUrl = BASE_URL + "new/shuffle/?deck_count=1"
+const newDeckUrl = BASE_URL + "/new/shuffle/?deck_count=1"
 let deckId = undefined
+
+const newDeckBtn = document.getElementById("newDeckBtn")
+const hitBtn = document.getElementById("hitBtn")
+
+const pickedCardsEl = document.getElementById("pickedCards")
+
+hitBtn.disabled = true
 
 function getNewDeck(){
     fetch(newDeckUrl)
         .then(response => response.json())
         .then(deckData => {
             deckId = deckData.deck_id
+            newDeckBtn.disabled = true
+            hitBtn.disabled = false
         })
 }
 
-function pickCards(){
+function pickCard(){
     if(!deckId){
-        console.error("No deck id")
+        console.error("Nincs paklink!")
         return
     }
+    let kep1 = document.createElement("img")
+    kep1.src = "https://deckofcardsapi.com/static/img/back.png"
+    let kep2 = document.createElement("img")
+    kep2.src = "https://deckofcardsapi.com/static/img/back.png"
+
+    pickedCardsEl.appendChild(kep1)
+    pickedCardsEl.appendChild(kep2)
+
     fetch(BASE_URL + `/${deckId}/draw/?count=2`)
+    .then(response => response.json())
+    .then(jsonRes => {
+        kep1.src = jsonRes.cards[0].image
+        kep2.src = jsonRes.cards[1].image
+        
+    })
 }
